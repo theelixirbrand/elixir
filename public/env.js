93c14703a25839ae8da43efec99ce06093b4d7c0
Kharay33455 <shakibkhan276337@gmail.com>
2025-04-27 02:16:04 +0100

    last

--- public/env.js
+++ public/env.js
@@ -1,7 +1,7 @@
 window.env = {
-  "REACT_APP_BH": "http://127.0.0.1:8000",
-  "REACT_APP_HSH": "VIEW CATALOUGE",
-  "REACT_APP_CN": "CHERON DEV",
-  "REACT_APP_ATCB": "rgba(50, 50, 0, 0.5)",
-  "REACT_APP_CCB": "#AACFA0"
+  "REACT_APP_BH": "https://elixirrest.pythonanywhere.com",
+  "REACT_APP_HSH": "EXPLORE EXLIXIR",
+  "REACT_APP_CN": "ELIXIR",
+  "REACT_APP_ATCB": "rgba(150, 21, 107, 0.5)",
+  "REACT_APP_CCB": "#96156B"
 };
