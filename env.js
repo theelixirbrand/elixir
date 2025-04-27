a1122bda9bcd8454fc9ac79d5ae1229130391661
theelixirbrand <theeliixiirbrand@gmail.com>
2025-04-27 01:30:11 +0000

    Updates

---
+++ env.js
@@ -0,0 +1,7 @@
+window.env = {
+  "REACT_APP_BH": "https://elixirrest.pythonanywhere.com",
+  "REACT_APP_HSH": "EXPLORE EXLIXIR",
+  "REACT_APP_CN": "ELIXIR",
+  "REACT_APP_ATCB": "rgba(150, 21, 107, 0.5)",
+  "REACT_APP_CCB": "#96156B"
+};
